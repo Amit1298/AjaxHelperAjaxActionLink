df18e59535f12c188ca4792e79b9e741d7662f86
Amit <amitkr24031995@gmail.com>
2022-04-22 16:43:56 +0530

    Adding AjaxHelperAjaxActionLink

---
+++ AjaxHelperAjaxActionLink/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using AjaxHelperAjaxActionLink.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AjaxHelperAjaxActionLink.Controllers
+{
+    public class HomeController : Controller
+    {
+        EmployeeDBEntities db = new EmployeeDBEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AllEmployees()
+        {
+            var data = db.employees.ToList();
+            return PartialView("_Employees",data);
+        }
+    }
+}
